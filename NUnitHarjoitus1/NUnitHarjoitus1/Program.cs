739913ac0716b7633769d596615acd8a492d8a70
Turo <turo.nylund@outlook.com>
2021-09-17 11:12:46 +0300

    Lisätty yksikkötestiharjoitus toisena projektina repoon

---
+++ NUnitHarjoitus1/NUnitHarjoitus1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NUnitHarjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Terve yksikkötestiharjoitus!");
+        }
+    }
+}
