79538c166758248402bb3567ca8aca35990884b9
Turo <turo.nylund@outlook.com>
2021-09-17 11:23:52 +0300

    lisäsin pisteen ohjelmaan

--- EsimerkkiLaskin/EsimerkkiLaskin/Program.cs
+++ EsimerkkiLaskin/EsimerkkiLaskin/Program.cs
@@ -7,7 +7,7 @@ namespace EsimerkkiLaskin
         static void Main(string[] args)
         {
             Console.WriteLine("Laskin ohjelman toinen versio");
-            Console.WriteLine("Laske 1+1=2");
+            Console.WriteLine("Laske 1+1=2.");
         }
     }
 }
